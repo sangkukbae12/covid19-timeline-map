ccfa04b6f45fdd7fd5c169044435d3df73cddb9a
sangkukbea <sangkukbea@naver.com>
2020-04-22 13:59:06 +0900

    add map slider

---
+++ src/components/MapSlider.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect, memo } from 'react';
+import {
+  Slider,
+  IconButton,
+  Grid,
+  Paper,
+  StylesProvider,
+} from '@material-ui/core';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import StopIcon from '@material-ui/icons/Stop';
+
+const MapSlider = ({ data, dateChangeHandler }) => {
+  const [date, setDate] = useState(null);
+  const [playing, setPlaying] = useState(false);
+  const [sliderValue, setSliderValue] = useState(0);
+  const [maxSliderValue, setMaxSliderValue] = useState(null);
+
+  useEffect(() => {
+    if (data) {
+      const d = data.map((d) => d.date);
+      setMaxSliderValue(d.length - 1);
+      setSliderValue(d.length - 1);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (data && sliderValue) {
+      const d = data.map((d) => d.date);
+      setDate(d[sliderValue]);
+      dateChangeHandler(date);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data, date, sliderValue]);
+
+  useEffect(() => {
+    let s;
+    if (playing) {
+      if (sliderValue === maxSliderValue) {
+        setPlaying(false);
+      } else {
+        s = setTimeout(() => {
+          setSliderValue((prev) => prev + 1);
+        }, 50);
+      }
+    }
+    return () => clearTimeout(s);
+  }, [playing, sliderValue, maxSliderValue]);
+
+  const getSliderValueTextFunc = (data) => (value) => {
+    if (data) {
+      const d = data.map((d) => d.date);
+      return d[value];
+    }
+  };
+
+  return (
+    <div style={{ position: 'fixed', left: '0', bottom: '0', width: '100vw' }}>
+      <Paper variant="outlined" square>
+        <Grid container alignItems="center">
+          <Grid item xs={1} sm={1}>
+            <IconButton
+              onClick={() => {
+                if (sliderValue === maxSliderValue) {
+                  setSliderValue(0);
+                }
+                setPlaying(!playing);
+              }}
+            >
+              {!playing ? <PlayArrowIcon /> : <StopIcon />}
+            </IconButton>
+          </Grid>
+          <Grid item xs={10} sm={10}>
+            <StylesProvider injectFirst>
+              <Slider
+                valueLabelFormat={getSliderValueTextFunc(data)}
+                // getAriaValueText={getSliderValueTextFunc(data)}
+                aria-labelledby="discrete-slider"
+                valueLabelDisplay="auto"
+                onChange={(event, newValue) => {
+                  setSliderValue(newValue);
+                }}
+                value={sliderValue}
+                step={1}
+                marks
+                min={0}
+                max={maxSliderValue}
+              />
+            </StylesProvider>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+};
+
+export default memo(MapSlider);
