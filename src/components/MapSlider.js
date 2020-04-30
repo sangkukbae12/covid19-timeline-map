90d2379e817ddcdb10cde379f731051f6887e416
sangkukbea <sangkukbea@naver.com>
2020-05-01 08:36:30 +0900

    modified slider css

--- src/components/MapSlider.js
+++ src/components/MapSlider.js
@@ -1,19 +1,27 @@
 import React, { useState, useEffect, memo } from 'react';
-import {
-  Slider,
-  IconButton,
-  Grid,
-  Paper,
-  StylesProvider,
-} from '@material-ui/core';
+import { Slider, IconButton, Grid, Paper, makeStyles } from '@material-ui/core';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import StopIcon from '@material-ui/icons/Stop';
+
+const useStyles = makeStyles((theme) => ({
+  valueLabel: {
+    backgroundColor: 'transparent',
+    color: theme.palette.secondary.main,
+  },
+  rail: {
+    backgroundColor: '#bfbfbf',
+  },
+  mark: {
+    backgroundColor: '#bfbfbf',
+  },
+}));
 
 const MapSlider = ({ data, dateChangeHandler }) => {
   const [date, setDate] = useState(null);
   const [playing, setPlaying] = useState(false);
   const [sliderValue, setSliderValue] = useState(0);
   const [maxSliderValue, setMaxSliderValue] = useState(null);
+  const classes = useStyles();
 
   useEffect(() => {
     if (data) {
@@ -70,22 +78,24 @@ const MapSlider = ({ data, dateChangeHandler }) => {
             </IconButton>
           </Grid>
           <Grid item xs={10} sm={10}>
-            <StylesProvider injectFirst>
-              <Slider
-                valueLabelFormat={getSliderValueTextFunc(data)}
-                // getAriaValueText={getSliderValueTextFunc(data)}
-                aria-labelledby="discrete-slider"
-                valueLabelDisplay="auto"
-                onChange={(event, newValue) => {
-                  setSliderValue(newValue);
-                }}
-                value={sliderValue}
-                step={1}
-                marks
-                min={0}
-                max={maxSliderValue}
-              />
-            </StylesProvider>
+            <Slider
+              classes={{
+                valueLabel: classes.valueLabel,
+                rail: classes.rail,
+                mark: classes.mark,
+              }}
+              valueLabelFormat={getSliderValueTextFunc(data)}
+              aria-labelledby="discrete-slider"
+              valueLabelDisplay="auto"
+              onChange={(event, newValue) => {
+                setSliderValue(newValue);
+              }}
+              value={sliderValue}
+              step={1}
+              marks
+              min={0}
+              max={maxSliderValue}
+            />
           </Grid>
         </Grid>
       </Paper>
